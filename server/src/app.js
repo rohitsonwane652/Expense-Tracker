0994477fa2862c4d1183d5319e75484917a654c7
rohitsonwane652 <rohitsonwane652@gmail.com>
2022-04-06 19:17:02 +0530

    database connected

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const dbConnect = require('./config/dbConnect')
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send("Hello Badhir");
-})
+});
+
+dbConnect();
 
 module.exports = app;
