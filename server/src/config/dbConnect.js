db42b193113e71579a6abe221ee1fb90598825fe
rohitsonwane652 <rohitsonwane652@gmail.com>
2022-04-11 18:03:35 +0530

    password crypted middleware added

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const dbConnect = async ()=>{
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}`);
-        console.log(`Connected Succesefully`);
+        console.log('mongodb connected succesfully');
     } catch (error) {
         console.log(`Error ${error.message}`)
     }
